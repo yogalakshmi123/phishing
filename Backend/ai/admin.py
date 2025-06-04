8884204ffdc3037eaf8738cd092d3d14b9a50237
yogalakshmi123 <e0223027@sriher.edu.in>
2025-06-04 23:14:59 +0530

    update admin in django

--- Backend/ai/admin.py
+++ Backend/ai/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import Useform
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "password", "age", "qualification", "level")
+
+admin.site.register(Useform, UserAdmin)
+
--- Backend/ai/models.py
+++ Backend/ai/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Useform(models.Model):
-    
+
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
@@ -11,6 +11,8 @@ class Useform(models.Model):
     qualification = models.CharField(max_length=100)
     level = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.name} {self.email} {self.password} {self.age} {self.qualification} {self.level}"
 
     class Meta:
 
